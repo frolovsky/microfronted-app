786ecd2b10b5ef0bd69238f8f0110cb62468c834
Алексей Фроловский (Тагес Джамп) <aleksei.frolovskii@tages.ru>
2023-09-11 15:41:38 +0300

    refactor: remove old codebase

--- main-app/src/store/user/getters.ts
+++
@@ -1,9 +0,0 @@
-import { GetterTree } from 'vuex';
-import { IUser, IUserState } from '@/store/user/types';
-import { IRootState } from '@/store/store.types';
-
-export const getters: GetterTree<IUserState, IRootState> = {
-  userData(state: IUserState): IUser | null {
-    return state.userData;
-  },
-};
--- main-app/src/store/user/mutations.ts
+++
@@ -1,4 +0,0 @@
-import { MutationTree } from 'vuex';
-import { IUserState } from '@/store/user/types';
-
-export const mutations: MutationTree<IUserState> = {};
